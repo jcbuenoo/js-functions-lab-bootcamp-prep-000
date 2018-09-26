f96d9bfe9fb1608bbe335377166b9cfbfbb8c587
Juan <jcbuenolopez@gmail.com>
2018-09-26 15:49:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code below!
+// write your code below //
+function happyHolidays() {
+  return "Happy holidays!"
+}
+// ------------
+function happyHolidaysTo (name) {
+  return 'Happy holidays, you!'
+}
+// ------------
+function happyHolidayTo (Birthday, Juan) {
+  return 'Happy Independence Day, you!'
+}
+
+function holidayCountdown (, days) {
+  return "It's ${10} days until ${Christmas}"
+}
